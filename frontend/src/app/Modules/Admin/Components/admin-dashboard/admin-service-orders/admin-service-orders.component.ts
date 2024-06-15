d885d1b355ed708c7e8eb67c1dd7da84fa09ac4c
Exodus <tea418pot@gmail.com>
2024-06-15 16:14:14 +0200

    refactor: unused imports cleared

--- frontend/src/app/Modules/Admin/Components/admin-dashboard/admin-service-orders/admin-service-orders.component.ts
+++ frontend/src/app/Modules/Admin/Components/admin-dashboard/admin-service-orders/admin-service-orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ServiceOrder, User } from "../../../../../Models/Models";
+import { ServiceOrder } from "../../../../../Models/Models";
 import { HttpClient } from "@angular/common/http";
 import { finalize } from "rxjs";
 
